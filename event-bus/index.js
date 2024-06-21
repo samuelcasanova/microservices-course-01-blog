f706796a64d204cacec0d23d886f2e73ef048943
samuel.casanova <samuel.casanova@immfly.com>
2024-06-21 07:21:25 +0200

    change localhost to cluster ip domain names

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,22 +1,30 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   const endpoints = [
-    'http://localhost:4000/events',
-    'http://localhost:4001/events',
-  ];
+    'http://posts-clusterip-srv:4000/events',
+    // 'http://localhost:4001/events',
+    // 'http://localhost:4002/events',
+    // 'http://localhost:4003/events',
+  ]
   for(const endpoint of endpoints) {
-    console.log(`Sending event to ${endpoint}`, req.body);
-    axios.post(endpoint, req.body)
+    console.log(`Sending event to ${endpoint}`, req.body)
+    try {
+      await axios.post(endpoint, req.body)
+      console.info('Successfully sent event')
+    }
+    catch (error) {
+      console.error('Error sending event', error)
+    }
   }
-  res.status(201).send({});
+  res.status(201).send({})
 });
 
 app.listen(4005, () => {
-  console.log('Events application listening on port 4005...')
+  console.log('Events application v2 listening on port 4005...')
 })
--- posts/index.js
+++ posts/index.js
@@ -1,25 +1,37 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser')
-const { randomBytes } = require('crypto');
-const cors = require('cors');
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+const axios = require('axios')
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 app.use(cors())
 
 const posts = {}
 
 app.get('/posts', (req, res) => {
-  res.send(posts);
-});
+  res.send(posts)
+})
 
-app.post('/posts', (req, res) => {
-  const id = randomBytes(4).toString('hex');
-  const { title } = req.body;
-  posts[id] = { id, title };
-  res.status(201).send(posts[id]);
-});
+app.post('/posts', async (req, res) => {
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+  posts[id] = { id, title }
+  try {
+    await axios.post('http://event-bus-srv:4005/events', { type: 'PostCreated', data: { id, title }})
+    console.info('Successfully created post and emitted the event')
+  } catch (error) {
+    console.error('Error trying to emit the event', error)
+  }
+  res.status(201).send(posts[id])
+})
+
+app.post('/events', (req, res) => {
+  console.info('Received event with payload', req.body)
+  res.status(200).send()
+})
 
 app.listen(4000, () => {
-  console.log('Posts application v1 listening on port 4000...')
+  console.log('Posts application v4 listening on port 4000...')
 })
